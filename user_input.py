93a9396cd636df943045113831f9c06c134e6180
AbbyGikunju <abigaelgikunju17@gmail.com>
2024-08-19 16:32:08 +0300

    User input code in python

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("Please provide me with your current location: ")
+
+print("Your name is " + name + ", you are " + age + " years old" + " and your location is " + location)
